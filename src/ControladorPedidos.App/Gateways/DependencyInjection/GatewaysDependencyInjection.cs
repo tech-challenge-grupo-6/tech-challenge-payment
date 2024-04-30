681ddf91c97b687d2fed766194d5b6172991df44
Victor <viih.higa@gmail.com>
2024-04-29 23:06:54 -0300

    Initial commit

---
+++ src/ControladorPedidos.App/Gateways/DependencyInjection/GatewaysDependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using ControladorPedidos.App.Entities.Repositories;
+
+namespace ControladorPedidos.App.Gateways.DependencyInjection;
+
+public static class GatewaysDependencyInjection
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IPedidoRepository, PedidoRepository>();
+        services.AddScoped<IPagamentoRepository, PagamentoRepository>();
+        services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+        return services;
+    }
+}
---
+++ src/ControladorPedidos.App/UseCases/DependencyInjection/UseCasesDependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using ControladorPedidos.App.Contracts;
+
+namespace ControladorPedidos.App.UseCases.DependencyInjection;
+
+public static class UseCasesDependencyInjection
+{
+    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        services.AddScoped<IPagamentoUseCase, PagamentoUseCase>();
+
+        return services;
+    }
+}
