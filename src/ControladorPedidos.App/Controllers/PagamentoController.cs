e3857e4544079354f2fd4b790314a007fa96f3a2
Victor Higa <viih.higa@gmail.com>
2024-05-08 00:25:44 -0300

    feat: implementa endpoint que retorna status do pedido

--- src/ControladorPedidos.App/Controllers/PagamentoController.cs
+++ src/ControladorPedidos.App/Controllers/PagamentoController.cs
@@ -12,6 +12,7 @@ public class PagamentoController(IPagamentoUseCase pagamentoUseCase, ILogger<Pag
 {
 
     private string? token;
+    private bool statusDoPedido = false;
 
     /// <summary>
     /// Realiza pagamento do pedido
@@ -103,6 +104,42 @@ public class PagamentoController(IPagamentoUseCase pagamentoUseCase, ILogger<Pag
         {
             logger.LogError(ex, "Erro ao efetuar pagamento do pedido {PedidoId}", pedidoId);
             return BadRequest($"Erro ao efetuar pagamento do pedido {pedidoId}");
+        }
+    }
+
+    /// <summary>
+    /// Verifica o status do pagamento do pedido
+    /// </summary>
+    /// <param name="pedidoId">Id do pedido</param>
+    /// <response code="200">Retorna se pagamento foi aprovado ou não.</response>
+    /// <response code="400">Bad request.</response>
+    /// <response code="404">Não encontrado.</response>
+    /// <response code="500">Erro interno.</response>
+    //[Authorize]
+    [HttpGet("status/{pedidoId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetStatus(Guid pedidoId)
+    {
+        try
+        {
+            logger.LogInformation("Consultando status do pedido {PedidoId}", pedidoId);
+            statusDoPedido = await pagamentoUseCase.ObterStatusDoPedidoAsync(pedidoId);
+            var successResponse = new { id = pedidoId, status = statusDoPedido };
+            return Ok(successResponse);
+        }
+        catch (NotFoundException e)
+        {
+            logger.LogError(e.Message);
+            var errorResponse = new { id = pedidoId, status = statusDoPedido };
+            return NotFound(errorResponse);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Erro ao tentar consultar o status de pagamento do pedido {PedidoId}", pedidoId);
+            return BadRequest($"Erro ao tentar consultar o status de pagamento do pedido {pedidoId}");
         }
     }
 }
