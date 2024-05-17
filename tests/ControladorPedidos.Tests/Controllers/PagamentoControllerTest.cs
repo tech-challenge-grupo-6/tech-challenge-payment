69e1025488883411ad1a21353c55c30e40190dc8
Victor Higa <viih.higa@gmail.com>
2024-05-16 23:32:17 -0300

    feat: testes

--- tests/ControladorPedidos.Tests/Controllers/PagamentoControllerTest.cs
+++ tests/ControladorPedidos.Tests/Controllers/PagamentoControllerTest.cs
@@ -2,22 +2,31 @@ using ControladorPedidos.App.Contracts;
 using ControladorPedidos.App.Controllers;
 using ControladorPedidos.App.Entities;
 using ControladorPedidos.App.Presenters;
+using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 
 namespace ControladorPedidos.Tests;
 public class PagamentoControllerTest
 {
-    private readonly IPagamentoUseCase pagamentoUseCase;
-
-    private readonly ILogger<PagamentoController> logger;
+    private readonly IPagamentoUseCase _pagamentoUseCase;
+    private readonly ILogger<PagamentoController> _logger;
+    private readonly PagamentoController _controller;
 
     public PagamentoControllerTest()
     {
-        pagamentoUseCase = Substitute.For<IPagamentoUseCase>();
-        logger = Substitute.For<ILogger<PagamentoController>>();
+        _pagamentoUseCase = Substitute.For<IPagamentoUseCase>();
+        _logger = Substitute.For<ILogger<PagamentoController>>();
+        _controller = new PagamentoController(_pagamentoUseCase, _logger)
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            }
+        };
     }
 
     [Fact]
@@ -26,16 +35,7 @@ public class PagamentoControllerTest
         // Arrange
         var pedidoId = Guid.NewGuid();
         var token = "Bearer teste";
-        var controller = new PagamentoController(pagamentoUseCase, logger)
-        {
-            ControllerContext = new ControllerContext
-            {
-                HttpContext = new DefaultHttpContext
-                {
-                    Request = { Headers = { ["Authorization"] = token } }
-                }
-            }
-        };
+        var controller = _controller;
 
         // Act
         var result = await controller.Put(pedidoId);
@@ -46,12 +46,35 @@ public class PagamentoControllerTest
     }
 
     [Fact]
+    public async Task Put_ReturnsBadRequest_WhenPaymentBadRequest()
+    {
+        // Arrange
+        var pedidoId = Guid.NewGuid();
+        var token = "Bearer token";
+        _controller.HttpContext.Request.Headers["Authorization"] = token;
+        var exception = new Exception("Pagamento falhou");
+        _pagamentoUseCase.EfetuarMercadoPagoQRCodeAsync(pedidoId, token)
+            .Throws(exception);
+
+        // Act
+        var result = await _controller.Put(pedidoId);
+
+        // Assert
+        var badRequestResult = result as BadRequestObjectResult;
+        badRequestResult.Should().NotBeNull();
+
+        // Verify 
+        var statusResult = Assert.IsType<BadRequestObjectResult>(result);
+        Assert.Equal(400, statusResult.StatusCode);
+    }
+
+    [Fact]
     public async Task Get_ConsultarPagamento_DeveRetornarOk()
     {
         // Arrange
         var pedidoId = Guid.NewGuid();
-        var controller = new PagamentoController(pagamentoUseCase, logger);
-        pagamentoUseCase.ConsultarPagamentoPeloPedido(pedidoId).Returns(new Pagamento());
+        var controller = new PagamentoController(_pagamentoUseCase, _logger);
+        _pagamentoUseCase.ConsultarPagamentoPeloPedido(pedidoId).Returns(new Pagamento());
 
         // Act
         var result = await controller.Get(pedidoId);
@@ -62,13 +85,33 @@ public class PagamentoControllerTest
     }
 
     [Fact]
+    public async Task Get_ConsultarPagamento_DeveRetornarBadRequest()
+    {
+        // Arrange
+        var pedidoId = Guid.NewGuid();
+        var exception = new Exception("Pagamento falhou");
+        _pagamentoUseCase.ConsultarPagamentoPeloPedido(pedidoId).Throws(exception);
+
+        // Act
+        var result = await _controller.Get(pedidoId);
+
+        // Assert
+        var badRequestResult = result as BadRequestObjectResult;
+        badRequestResult.Should().NotBeNull();
+
+        // Verify 
+        var statusResult = Assert.IsType<BadRequestObjectResult>(result);
+        Assert.Equal(400, statusResult.StatusCode);
+    }
+
+    [Fact]
     public async Task Post_ConcluirPagamento_DeveRetornar201Created()
     {
         // Arrange
         var pedidoId = Guid.NewGuid();
         var token = "Bearer teste";
         var pagamentoWebhookDto = new PagamentoWebhookDto(pedidoId, true, null);
-        var controller = new PagamentoController(pagamentoUseCase, logger)
+        var controller = new PagamentoController(_pagamentoUseCase, _logger)
         {
             ControllerContext = new ControllerContext
             {
@@ -78,7 +121,7 @@ public class PagamentoControllerTest
                 }
             }
         };
-        pagamentoUseCase.ConcluirPagamento(pedidoId, true, null, token).Returns(pedidoId);
+        _pagamentoUseCase.ConcluirPagamento(pedidoId, true, null, token).Returns(pedidoId);
 
         // Act
         var result = await controller.Post(pagamentoWebhookDto);
@@ -89,12 +132,46 @@ public class PagamentoControllerTest
     }
 
     [Fact]
+    public async Task Post_ConcluirPagamento_DeveRetornarBadRequest()
+    {
+        // Arrange
+        var pedidoId = Guid.NewGuid();
+        var token = "Bearer token";
+        var pagamentoWebhookDto = new PagamentoWebhookDto(pedidoId, true, "Erro");
+        var controller = new PagamentoController(_pagamentoUseCase, _logger)
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    Request = { Headers = { ["Authorization"] = token } }
+                }
+            }
+        };
+        var exception = new Exception("Pagamento falhou");
+
+        _pagamentoUseCase.ConcluirPagamento(pedidoId, true, null, token)
+            .Throws(exception);
+
+        // Act
+        var result = await controller.Post(pagamentoWebhookDto);
+
+        // Assert
+        var badRequestResult = result as BadRequestObjectResult;
+        badRequestResult.Should().NotBeNull();
+
+        // Verify 
+        var statusResult = Assert.IsType<BadRequestObjectResult>(result);
+        Assert.Equal(400, statusResult.StatusCode);
+    }
+
+    [Fact]
     public async Task GetStatus_ObterStatusDoPedido_DeveRetornarOk()
     {
         // Arrange
         var pedidoId = Guid.NewGuid();
-        var controller = new PagamentoController(pagamentoUseCase, logger);
-        pagamentoUseCase.ObterStatusDoPedidoAsync(pedidoId).Returns(true);
+        var controller = new PagamentoController(_pagamentoUseCase, _logger);
+        _pagamentoUseCase.ObterStatusDoPedidoAsync(pedidoId).Returns(true);
 
         // Act
         var result = await controller.GetStatus(pedidoId);
@@ -103,5 +180,26 @@ public class PagamentoControllerTest
         var okResult = Assert.IsType<OkObjectResult>(result);
         Assert.Equal(200, okResult.StatusCode);
     }
+
+    [Fact]
+    public async Task GetStatus_ObterStatusDoPedido_DeveRetornarBadRequest()
+    {
+        // Arrange
+        var pedidoId = Guid.NewGuid();
+        var exception = new Exception("Pagamento falhou");
+        _pagamentoUseCase.ObterStatusDoPedidoAsync(pedidoId).Throws(exception);
+
+        // Act
+        var result = await _controller.GetStatus(pedidoId);
+
+        // Assert
+        var badRequestResult = result as BadRequestObjectResult;
+        badRequestResult.Should().NotBeNull();
+
+        // Verify 
+        var statusResult = Assert.IsType<BadRequestObjectResult>(result);
+        Assert.Equal(400, statusResult.StatusCode);
+    }
+
 }
 
